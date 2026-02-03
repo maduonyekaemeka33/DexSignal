478d32e86f73c4122c377992309480281d00e56c
maduonyekaemeka33 <maduonyekaemeka@gmail.com>
2026-02-03 07:09:15 +0100

    feat: render MemeCoinDashboard and DexScreenerIframe in root

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import MemeCoinDashboard from "./components/MemeCoinDashboard";
+import DexScreenerIframe from "./components/DexScreenerIframe";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MemeCoinDashboard />
+    <DexScreenerIframe />
   </React.StrictMode>
 );

64a32c1ab0eebf9f8608af400d1388a360b49e30
maduonyekaemeka33 <maduonyekaemeka@gmail.com>
2026-02-03 04:13:10 +0100

    Add App entry point and render MemeCoinDashboard

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MemeCoinDashboard from "./components/MemeCoinDashboard";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <MemeCoinDashboard />
+    </div>
+  );
+}
+
+export default App;
